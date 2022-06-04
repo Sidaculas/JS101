a7de171ac340336402f61e90e82979e81d1bed68
Sidaculas <shafaat.siddhi19@gmail.com>
2022-06-05 00:20:18 +0600

    added full description about data types and variable types.

--- JS/datatypes.js
+++ JS/datatypes.js
@@ -1,6 +1,23 @@
-// Js has 3 types of data: constants as const (can use it only after assigning, also a block statement),
-// variables as var (can define anywhere and use it), and let (block statement, best for loops).
+// There are 6 Primitive data types in JavaScript:
 
-const one = 1;
-let two = 2;
-var three = 3;
+// String, number, boolean, null, undefined, symbol, bigint;
+// String is a sequence of characters.
+// Number is a number.
+// Boolean is either true or false.
+// Null is an empty value.
+// Undefined is a value that has not been assigned a value.
+// Symbol is a unique value.
+// BigInt is a 64-bit integer.
+
+// there are 3 variables in JavaScript:
+// var, let, const
+// var is a variable that can be reassigned. A variable can be declared with var, but it can also be declared with let or const.
+// let is a variable that can be reassigned. it is a block-scoped variable, which means that it is available only within the block in which it is defined.
+// const is a variable that cannot be reassigned. 
+
+
+// objects created with const are immutable. but object values can be changed.
+// For example, if we have a const object, and we want to change the value of a property, we can do so, but we cannot reassign the object itself. 
+
+const obj = {a : 6};
+obj.a = 7;
