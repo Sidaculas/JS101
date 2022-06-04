70b936c4b6bf6127cc3999423d6024a0d85a9eab
Sidaculas <shafaat.siddhi19@gmail.com>
2022-06-04 23:56:52 +0600

    added description about data types in comment

--- JS/datatypes.js
+++ JS/datatypes.js
@@ -1,4 +1,5 @@
-// Js has 3 types of data: constants as const, variables as var, and let.
+// Js has 3 types of data: constants as const (can use it only after assigning, also a block statement),
+// variables as var (can define anywhere and use it), and let (block statement, best for loops).
 
 const one = 1;
 let two = 2;
